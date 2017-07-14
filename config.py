6c5947687c5eedbbb68e15b1df5267928d0e75c9
dexiou <imuser@qq.com>
2017-07-14 18:22:27 +0800

    Initial commit

---
+++ config.py
@@ -0,0 +1,8 @@
+# 配置DEBUG模式，测试阶段热部署
+DEBUG = True
+# 配置上传目录
+UPLOAD_FOLDER = '/tmp/uploads'
+# 最大文件体积
+MAX_CONTENT_LENGTH = 2 * 1024 * 1024
+# 预防跨站伪造
+SECRET_KEY = 'P4S5W0RD'
---
+++ fupin.py
@@ -0,0 +1,70 @@
+import config, os
+import random, string
+from flask import Flask, request, url_for, send_from_directory
+from werkzeug.utils import secure_filename
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, Length, Email
+
+
+# 配置文件类型
+ALLOWED_EXTENSIONS = set(['xls', 'xlsx', 'png', 'jpg'])
+
+app = Flask(__name__)
+
+# 加载配置文件
+app.config.from_object(config)
+
+# 输出到客户端的字符串
+html = '''
+    <!DOCTYPE html>
+    <title>Upload File</title>
+    <h1>图片上传</h1>
+    <form method=post enctype=multipart/form-data action="/">
+         <input type=file name=file>
+         <input type=submit value=上传>
+    </form>
+    '''
+
+# 判断上传的文件类型
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
+# 返回token，用于设置session
+@app.route('/token')
+def gen_token():
+    return ''.join(random.sample(string.ascii_letters + string.digits, 8))
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
+
+# 响应根路由
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        print(file)
+        print(file.filename.rsplit('.', 1))
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            file_url = url_for('uploaded_file', filename=filename)
+            print(file_url)
+            return html + '<br><img src=' + file_url + '>'
+    return html
+
+
+@app.route('/index')
+def index():
+    return send_from_directory('static','index.html')
+
+
+
+if __name__ == '__main__':
+    app.run()
